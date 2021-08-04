076aeecab024187a5b0ae076f6a177f598292ff6
Don-Laliberte <donhenrylaliberte@gmail.com>
2021-08-03 22:51:28 -0600

    Made basic TreeException

--- A4_BSTWordTracker/src/exceptions/TreeException.java
+++ A4_BSTWordTracker/src/exceptions/TreeException.java
@@ -1,5 +1,15 @@
 package exceptions;
 
 public class TreeException extends Exception {
-
+	public TreeException() {
+		super();
+	}
+	
+	/**
+	 * Constructor that takes message parameter
+	 * @param message sent to console to explain specific cause of thrown error
+	 */
+	public TreeException(String message) {
+		super(message);
+	}
 }
