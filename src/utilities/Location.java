bef9352fa9272d518ad57518db248ecd881dd53b
Don-Laliberte <donhenrylaliberte@gmail.com>
2021-08-07 01:59:48 -0600

    Implemented Location Object

--- src/utilities/Location.java
+++ src/utilities/Location.java
@@ -2,4 +2,30 @@ package utilities;
 
 public class Location {
 
+	private String filename;
+	private int line;
+	
+	
+	
+	public Location() {
+	}
+
+	public Location(String filename, int line) {
+		super();
+		this.filename = filename;
+		this.line = line;
+	}
+	
+	public String getFilename() {
+		return filename;
+	}
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+	public int getLine() {
+		return line;
+	}
+	public void setLine(int line) {
+		this.line = line;
+	}
 }
